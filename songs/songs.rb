318dc2d8a98edc9adeef4b1f64f4b03c2a4b5d30
caesarfeta <adamtavares@gmail.com>
2014-11-03 20:48:59 -0500

    Song page template is working!  A few improvements and it's ready for prime time.

--- songs/songs.rb
+++ songs/songs.rb
@@ -1,8 +1,9 @@
 require 'yaml'
+require 'erubis'
 
 def load_song( song )
   # Check for files that match song in csml
-  fnames = Dir.entries('csml')
+  fnames = Dir.entries("csml")
   matches = []
   fnames.each do |name|
     if name.include? song
@@ -10,18 +11,17 @@ def load_song( song )
     end
   end
   # Get the content of matched files
-  content = {}
-  content[song] = {}
+  data = {}
   matches.each do |match|
     lang = match.split(/\./)
     # Get the text of the matched file
-    content[song][lang[1]] = load_file(match)
+    data[lang[1]] = load_file(match)
   end
-  puts content.inspect
+  data
 end
 
 def chorus?( line )
-  if line[0,1] == '*'
+  if line[0,1] == "*"
     return true
   end
   false
@@ -37,33 +37,41 @@ end
 
 def load_file( file )
   # Initialize config
-  config = {}
+  data = {}
   stanza = nil
-  config["stanzas"] = []
+  data["stanzas"] = []
   # Get the text from the file
   text = File.read("csml/#{file}")
   text.each_line do |line|
     # Get rid of newlines
     line.delete!("\n")
     # Find the title
-    if line[0,1] == '#'
-      config['title'] = cleanup( line )
+    if line[0,1] == "#"
+      data["title"] = cleanup( line )
       next
     end
     # Check for empty lines
     if line == ""
       stanza = stanza == nil ? 0 : stanza+1
-      config["stanzas"][stanza] = []
+      data["stanzas"][stanza] = []
       next
     end
     # Add the line
     item = { "chorus" => chorus?(line), "line" => cleanup(line) }
-    config["stanzas"][stanza].push(item)
+    data["stanzas"][stanza].push(item)
   end
-  config
+  data
+end
+
+def parse_template( data )
+  erb = File.read("songs.html.erb")
+  output = Erubis::Eruby.new(erb).result(:data=>data)
+  File.open("../songs.html","w") { |f| f.write(output) }
 end
 
 config = YAML.load( File.read('config.yaml'))
+data = {}
 config['songs'].each do |song|
-  csml = load_song( song )
+  data[song] = load_song( song )
 end
+parse_template( data )
